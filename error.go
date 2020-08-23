1a9726d5d7225d1970295013d43ac8dfaf65f402
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-08-23 19:55:34 +0100

    adding new json response with error as root

--- error.go
+++ error.go
@@ -2,31 +2,73 @@ package nerror
 
 import "encoding/json"
 
-// ErrorMessage returns json:
+// SimpleErrorMessage returns json:
 /*
 	{
 		"message" : "Your json is wrong or something",
 		"code" : 500
 	}
 |*/
-type ErrorMessage struct {
+type SimpleErrorMessage struct {
 	Message string `json:"message"`
-	Code    int    `json:"code"`
+	Status  int    `json:"status"`
 }
 
-//SimpeErrorResponseWithCode returns ErrorMessage struct as an []bytes
+// SimpleErrorMessageV2 returns json:
+/*
+	{
+  		"error" : {
+    		"status" : 502,
+    		"message" : "Bad gateway."
+  		}
+	}
+|*/
+type SimpleErrorMessageV2 struct {
+	ErrorST SimpleErrorMessage `json:"error"`
+}
+
+//SimpeErrorResponseWithStatus returns SimpleErrorMessage struct as an []bytes
 /*
 	return json response in this format:
 
 	{
 		"message" : "Your json is wrong or something",
-		"code" : 500
+		"status" : 500
 	}
 
 	@return array of bytes of the json object
 	@return int http code status
 */
-func SimpeErrorResponseWithCode(code int, err error) ([]byte, error) {
-	JSONBody, errMarshal := json.Marshal(ErrorMessage{Message: err.Error(), Code: code})
+func SimpeErrorResponseWithStatus(status int, err error) ([]byte, error) {
+	JSONBody, errMarshal := json.Marshal(
+		SimpleErrorMessage{
+			Message: err.Error(),
+			Status:  status,
+		},
+	)
+
+	return JSONBody, errMarshal
+}
+
+//SimpeErrorResponseWithCodeV2 returns SimpleErrorMessageV2 struct as an []bytes
+/*
+	return json response in this format:
+
+	{
+		"message" : "Your json is wrong or something",
+		"status" : 500
+	}
+
+	@return array of bytes of the json object
+	@return int http status
+*/
+func SimpeErrorResponseWithCodeV2(status int, err error) ([]byte, error) {
+	JSONBody, errMarshal := json.Marshal(
+		SimpleErrorMessageV2{
+			ErrorST: SimpleErrorMessage{
+				Message: err.Error(),
+				Status:  status,
+			},
+		})
 	return JSONBody, errMarshal
 }
--- error_test.go
+++ error_test.go
@@ -7,12 +7,27 @@ import (
 
 func TestSimpeErrorResponseWithCode(t *testing.T) {
 	err1 := errors.New("math: square root of negative number")
-	statsErrJSONBody, err := SimpeErrorResponseWithCode(500, err1)
+	statsErrJSONBody, err := SimpeErrorResponseWithStatus(500, err1)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	errorJSON := `{"message":"math: square root of negative number","code":500}`
+	errorJSON := `{"message":"math: square root of negative number","status":500}`
+
+	if string(statsErrJSONBody) != errorJSON {
+		t.Fatalf("Responses are not equal %s with %s", string(statsErrJSONBody), errorJSON)
+	}
+
+}
+
+func TestSimpeErrorResponseWithCodeV2(t *testing.T) {
+	err1 := errors.New("math: square root of negative number")
+	statsErrJSONBody, err := SimpeErrorResponseWithCodeV2(500, err1)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	errorJSON := `{"error":{"message":"math: square root of negative number","status":500}}`
 
 	if string(statsErrJSONBody) != errorJSON {
 		t.Fatalf("Responses are not equal %s with %s", string(statsErrJSONBody), errorJSON)
